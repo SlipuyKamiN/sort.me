79e5b07bab7c74c6bf948d414c9b2f8753d3aedf
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-06-30 18:31:15 +0200

    upd: cities list

--- src/data/citiesList.js
+++ src/data/citiesList.js
@@ -1,32 +1,59 @@
+const BASE_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX';
+
 export const citiesList = [
   {
     cityID: 'Berlin',
     cityName: 'Berlin',
-    sheetLink:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vTxjfsLxX9Qsb7zXdRLrnADjvKpCooVO8_MUDF8sZaBFQj6gGMU6_HIe3T8SK6fRbH6S9iISgVUEoWi/pub?output=csv',
-  },
-  {
-    cityID: 'Warszaw',
-    cityName: 'Warszaw',
-    sheetLink:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQEKdT6hC1awC4pzOvFzBV2RCNZlcPid-Z5EwLDXj1x-qPwQNUOk0ckkpl-fkFFza9_mguWvuP_8thM/pub?output=csv',
+    sheetLink: `${BASE_URL}-1vTxjfsLxX9Qsb7zXdRLrnADjvKpCooVO8_MUDF8sZaBFQj6gGMU6_HIe3T8SK6fRbH6S9iISgVUEoWi/pub?output=csv`,
   },
   {
     cityID: 'Hamburg',
     cityName: 'Hamburg',
-    sheetLink:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vTN-JtaPhsO_hUM_xgHi6Hg3PNEUq7Uy82nHu3fIh9p-n7saoRcd-vG1beP8jYuJiKasPX7EXp4Fine/pub?output=csv',
-  },
-  {
-    cityID: 'Cologne',
-    cityName: 'Köln',
-    sheetLink:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSU2H_gR8xL2FV9sF7BX9rp1tuYhOxdo4rkBP63osGsHhKuhe8Z6KEjwkBao-Nvb1eVUKzgGCv8U6dI/pub?output=csv',
+    sheetLink: `${BASE_URL}-1vTN-JtaPhsO_hUM_xgHi6Hg3PNEUq7Uy82nHu3fIh9p-n7saoRcd-vG1beP8jYuJiKasPX7EXp4Fine/pub?output=csv`,
   },
   {
     cityID: 'Munich',
     cityName: 'München',
-    sheetLink:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vT-QrhttIZ3rmvWObjgCspeBn_FrpAVeqRbhhoORbPhAY7_d2zv0dd2y0Mg2QXIHXYccdn3OquDiKl2/pub?output=csv',
+    sheetLink: `${BASE_URL}-1vT-QrhttIZ3rmvWObjgCspeBn_FrpAVeqRbhhoORbPhAY7_d2zv0dd2y0Mg2QXIHXYccdn3OquDiKl2/pub?output=csv`,
+  },
+  {
+    cityID: 'Dresden',
+    cityName: 'Dresden',
+    sheetLink: `${BASE_URL}-1vQEKdT6hC1awC4pzOvFzBV2RCNZlcPid-Z5EwLDXj1x-qPwQNUOk0ckkpl-fkFFza9_mguWvuP_8thM/pub?output=csv`,
+  },
+  {
+    cityID: 'Cologne',
+    cityName: 'Köln',
+    sheetLink: `${BASE_URL}-1vSU2H_gR8xL2FV9sF7BX9rp1tuYhOxdo4rkBP63osGsHhKuhe8Z6KEjwkBao-Nvb1eVUKzgGCv8U6dI/pub?output=csv`,
+  },
+  {
+    cityID: 'Leipzig',
+    cityName: 'Leipzig',
+    sheetLink: `${BASE_URL}-1vTA2S7-LDgK5YTTPl__V6cHaLHoOPsHCq7FvxxmEKiGJdtjEMZ4McXiiF5WnMYhO5hcmSUeBln2J0Yh/pub?output=csv`,
+  },
+  {
+    cityID: 'Duesseldorf',
+    cityName: 'Düsseldorf',
+    sheetLink: `${BASE_URL}-1vSPoXc3n17OGOim0P8biu3H8zoRoR9bPgMpX0Q0PEsM9VJu5CmI-u4tiyAzACrUDi2Tl8vyBYUM7vkh/pub?output=csv`,
+  },
+  {
+    cityID: 'Dortmund',
+    cityName: 'Dortmund',
+    sheetLink: `${BASE_URL}-1vTWUyumIdreNzhRqwgiyn4q-dSH7_6nhmJjiJNSAo2_WwpC6SPTUCSXZ-ufrctvNDM2NfrToTp4vMQu/pub?output=csv`,
+  },
+  {
+    cityID: 'Essen',
+    cityName: 'Essen',
+    sheetLink: `${BASE_URL}-1vRH3r7OizRPHuHOhXD3AEvuCdr0vnSBKqFJtn8baElNe-lArx4UXpVD9TKa8hi8gFWOlyxVenseLDeH/pub?output=csv`,
+  },
+  {
+    cityID: 'Bremen',
+    cityName: 'Bremen',
+    sheetLink: `${BASE_URL}-1vSVM-Fzmjb5bdYiWBMcKQR7utGfVEyJk3dfm0RTL6yZKvES4lxWyZ8_09oFxrXg2Yo-O3Kp7QUbsrdN/pub?output=csv`,
+  },
+  {
+    cityID: 'Frankfurt-am-Main',
+    cityName: 'Frankfurt',
+    sheetLink: `${BASE_URL}-1vRCj1gZivsOLKXklUF4JVE2WfKPJxfl71hpxsOY0tRfyUcdtCMT93FYUQehiUbFxmIg1XJsOZNWWpv8/pub?output=csv`,
   },
 ];
